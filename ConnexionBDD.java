bca12ddc18696c7f2f51d4d83fb89d3dbd394884
Yann <ydauvin@enssat.fr>
2020-11-21 17:58:55 +0100

    test sur la bdd

--- ConnexionBDD.java
+++ ConnexionBDD.java
@@ -5,23 +5,35 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 public class ConnexionBDD{
-  public static void main(String[] args) {
-    Connection connexion;
-    try {
-      Class.forName("com.mysql.jdbc.Driver");
-      System.out.println("Initialising connection to Q8 Database");
-      connexion = DriverManager.getConnection("jdbc:mysql://mysql-nekoshine.alwaysdata.net/nekoshine_gl2020q8",
-      "nekoshine_user", "Q82020GL");
-      System.out.println("Connection to Q8 Database complete");
-      PreparedStatement requete = connexion.prepareStatement("INSERT INTO Test VALUES (?,?)");
-      requete.setInt(1, 42);
-      requete.setString(2, "Bonsoir paris");
-      requete.executeUpdate();
-      requete.close();
-    } catch (Exception e) {
-      System.err.println("Couldn't connect to DB : " + e.getMessage());
+    public static void main(String[] args) {
+        Connection connexion;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Initialising connection to Q8 Database");
+            connexion = DriverManager.getConnection("jdbc:mysql://mysql-nekoshine.alwaysdata.net/nekoshine_gl2020q8",
+            "nekoshine_user", "Q82020GL");
+            System.out.println("Connection to Q8 Database complete");
+
+            //Inserer une valeur
+            /*PreparedStatement requete = connexion.prepareStatement("INSERT INTO Test VALUES (?,?)");
+            requete.setInt(1, 666);
+            requete.setString(2, "Satan");
+            requete.executeUpdate();*/
+
+            //Recuperer la BDD
+            PreparedStatement requete = connexion.prepareStatement("SELECT * FROM Test");
+            ResultSet rs = requete.executeQuery();
+            while (rs.next()) {
+            System.out.print(rs.getString(1));
+            System.out.print(" | ");
+            System.out.println(rs.getString(2));
+            }
+
+            requete.close();
+        } catch (Exception e) {
+            System.err.println("Couldn't connect to DB : " + e.getMessage());
+        }
+
+        System.out.println("Getting connexion to Hostel database..");
     }
-    
-    System.out.println("Getting connexion to Hostel database..");
-  }
 }
