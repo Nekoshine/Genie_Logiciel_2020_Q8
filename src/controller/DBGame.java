5306101f0b3c8ffa2b7916a87b392cb0b8d33606
Yann <ydauvin@enssat.fr>
2020-12-03 20:13:46 +0100

    j'ai corriger des erreurs que j'ai fait

--- src/controller/DBGame.java
+++ src/controller/DBGame.java
@@ -47,7 +47,7 @@ public class DBGame {
     ArrayList<Game> gameList = new ArrayList<Game>();
     Boolean boolGame=false;
     try{
-      PreparedStatement requete = DBUser.getConnexion().prepareStatement("Select * from model.Game");
+      PreparedStatement requete = DBUser.getConnexion().prepareStatement("Select * from Game");
       ResultSet resultat = requete.executeQuery();
       if (resultat.next()) { // On itère chaque résultat
         if(resultat.getInt("ready")==1){ // On convertit le booleen car il est stocké comme un entier dans la base
