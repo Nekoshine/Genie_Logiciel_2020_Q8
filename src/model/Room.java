ac5bfbaa2d739077fe890a648a028b48a31ebe4f
C-Plantet <cplantet@enssat.fr>
2020-12-03 23:31:07 +0100

    Amélioration Ajout des salles + Design Création d'une classe ColorPerso pour ajouter des couleurs personnalisées

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,12 +1,15 @@
 package model;
 
 public class Room{
+
   private int id;
   private int idGame;
+
   public Room(int id,int idGame){
     this.id = id;
     this.idGame = idGame;
   }
+
   public int getId(){
     return this.id;
   }
---
+++ src/view/ColorPerso.java
@@ -0,0 +1,15 @@
+package view;
+
+import java.awt.*;
+
+public class ColorPerso extends Color {
+
+    public static ColorPerso choixJeu = new ColorPerso(243,215,39);
+    public static ColorPerso lancement = new ColorPerso(19,165,25);
+    public static ColorPerso gris = new ColorPerso(211,211,211);
+    public static  ColorPerso retour = new ColorPerso(180,40,20);
+
+    public ColorPerso(int r, int g, int b) {
+        super(r, g, b);
+    }
+}
