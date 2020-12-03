b2262cc2350e75942a54eecaf061dfc837e797e4
Nekoshine <uharry44@gmail.com>
2020-12-03 16:08:15 +0100

    Ajout des Objets de la BDD et Fonction de selection des jeux

---
+++ src/DBGame.java
@@ -0,0 +1,67 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.security.*;
+import java.io.*;
+import java.util.Arrays;
+/**
+* Classe qui va contenir toutes les méthodes liées a la DB pour un jeu
+*/
+public class DBGame {
+  private static Connection connexion;
+  
+  public DBGame(){
+    getConnexion();
+  }
+  /**
+  * Fonction qui va initialiser la connexion avec la BDD
+  * @return La connexion établie avec la bdd
+  */
+  public static Connection getConnexion(){
+    if (connexion==null){
+      try{
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Initialising connection to Q8 Database");
+        DBGame.connexion = DriverManager.getConnection("jdbc:mysql://mysql-nekoshine.alwaysdata.net/nekoshine_gl2020q8",
+        "nekoshine_user", "Q82020GL");
+        System.out.println("Connection to Q8 Database complete");
+        return connexion;
+      } catch ( SQLException e){
+        System.out.println("Couldn't connect to Q8 DB :" + e.getMessage());
+      }catch ( ClassNotFoundException e){
+        System.out.println("Couldn't connect to Q8 DB :" + e.getMessage());
+      }
+    }
+    System.out.println("Getting connexion to Q8 DB");
+    return connexion;
+  }
+  /**
+  * Fonction qui va récupérer les jeux dans la base de données et le stocker dans un ArrayList
+  * @return Liste de jeux
+  */
+  public static ArrayList<Game> getGames(){
+    ArrayList<Game> gameList = new ArrayList<Game>();
+    Boolean boolGame=false;
+    try{
+      PreparedStatement requete = DBUser.getConnexion().prepareStatement("Select * from Game");
+      ResultSet resultat = requete.executeQuery();
+      if (resultat.next()) { // On itère chaque résultat
+        if(resultat.getInt("ready")==1){ // On convertit le booleen car il est stocké comme un entier dans la base
+          boolGame=true;
+        }else{
+          boolGame=false;
+        }
+        gameList.add(new Game(resultat.getInt("id"), resultat.getString("titre"),
+        resultat.getInt("score"), resultat.getInt("idUser"),resultat.getInt("timer"),boolGame)); // On crée l'objet Game et on l'ajoute dans la liste
+      }
+      requete.close();
+      resultat.close();
+    } catch(SQLException e ){
+      System.err.println("Erreur requete connectUser: " + e.getMessage());
+    }
+    return gameList;
+  }  
+}
