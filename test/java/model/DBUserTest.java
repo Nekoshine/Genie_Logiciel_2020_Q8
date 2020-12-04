8f47e39eb6fe859597ed536a0f8aa91597b1055b
Yann <ydauvin@enssat.fr>
2020-12-04 14:33:40 +0100

    test d'injection

--- test/java/model/DBUserTest.java
+++ test/java/model/DBUserTest.java
@@ -12,31 +12,31 @@ class DBUserTest {
 
     @Test
     final void testInsertUser() {
-        boolean test = DBUser.insertUser(DBUserTest.GOOD_LOGIN, DBUserTest.GOOD_PASSWORD);
+        boolean test = DBUser.insertUser(GOOD_LOGIN, GOOD_PASSWORD);
         Assertions.assertTrue(test, "Echec de l'ajout du couple login/password à la BDD");
     }
 
     @Test
     final void testConnectGoodUserGoodPassword() {
-        boolean test = DBUser.connectUser(DBUserTest.GOOD_LOGIN, DBUserTest.GOOD_PASSWORD);
+        boolean test = DBUser.connectUser(GOOD_LOGIN, GOOD_PASSWORD);
         Assertions.assertTrue(test, "Le couple login/password n'a pas été trouvé dans la BDD");
     }
 
     @Test
     final void testConnectGoodUserWrongPassword() {
-        boolean test = DBUser.connectUser(DBUserTest.GOOD_LOGIN, DBUserTest.WRONG_PASSWORD);
+        boolean test = DBUser.connectUser(GOOD_LOGIN, WRONG_PASSWORD);
         Assertions.assertFalse(test, "La connexion a été possible avec le mauvais password");
     }
 
     @Test
     final void testConnectWrongUserGoodPassword() {
-        boolean test = DBUser.connectUser(DBUserTest.WRONG_LOGIN, DBUserTest.GOOD_PASSWORD);
+        boolean test = DBUser.connectUser(WRONG_LOGIN, GOOD_PASSWORD);
         Assertions.assertFalse(test, "La connexion a été possible avec un password associé à personne");
     }
 
     @Test
     final void testConnectWrongUserWrongPassword() {
-        boolean test = DBUser.connectUser(DBUserTest.WRONG_LOGIN, DBUserTest.WRONG_PASSWORD);
+        boolean test = DBUser.connectUser(WRONG_LOGIN, WRONG_PASSWORD);
         Assertions.assertFalse(test, "La connexion a été possible avec un login et un password non present dans la BDD");
     }
 
@@ -46,7 +46,7 @@ class DBUserTest {
         String login = "GoodLogin\" AND 1; -- ";
         String password ="*/GoodLogin";
         boolean test = DBUser.connectUser(login, password);
-        Assertions.assertFalse(test, "");
+        Assertions.assertFalse(test, "L'injection à l'aveugle a fonctionné");
     }
 
     @Test
@@ -55,7 +55,7 @@ class DBUserTest {
         String login = "GoodLogin\" AND 1; -- ";
         String password ="*/GoodLogin";
         boolean test = DBUser.connectUser(login, password);
-        Assertions.assertFalse(test, "");
+        Assertions.assertFalse(test, "L'injection a fonctionné");
     }
 
     @Test
