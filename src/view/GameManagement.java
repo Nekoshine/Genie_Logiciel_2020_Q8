85b24500022d1e283ebdb9a7da3ebfb17c25b169
Bastien <bastien.maubon@gmail.com>
2020-12-08 16:43:49 +0100

    fenêtre de gestion des jeux mise à jour

--- src/view/GameManagement.java
+++ src/view/GameManagement.java
@@ -1,33 +1,53 @@
+/* Interface développée par Bastien Maubon */
+
 package view;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameManagement extends JPanel implements ActionListener {
+    /*Final JPanel*/
     public JPanel windowNamePanel = new JPanel();
     public JPanel gameListPanel = new JPanel();
     public JPanel buttonAddGamePanel = new JPanel();
     public JPanel buttonReturnPanel = new JPanel();
+    public JScrollPane scrollGameListPanel = new JScrollPane(gameListPanel,
+            ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
     public GameManagement(){
 
+        /*WindowNamePanel set up*/
         JLabel windowName = new JLabel("MJ - Gestion des Jeux");
-        windowNamePanel.add(windowName);
-        windowNamePanel.setBackground(Color.white);
+        JPanel windowNameInsidePanel = new JPanel();
+        windowNameInsidePanel.add(windowName);
+        windowNameInsidePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+        windowNamePanel.setBorder(BorderFactory.createEmptyBorder(0,0,10,0));
+        windowNamePanel.setLayout(new BoxLayout(windowNamePanel,BoxLayout.LINE_AXIS));
+        windowNamePanel.add(windowNameInsidePanel);
+
+        scrollGameListPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
 
         gameListPanel.setLayout(new BoxLayout(gameListPanel, BoxLayout.PAGE_AXIS));
 
-        int nbGames = 3; //fonction pour récupérer nombre de Jeux enregistrés dans BdD
+        int nbGames = 14; //fonction pour récupérer nombre de Jeux enregistrés dans BdD
 
         for(int i = 1; i<=nbGames; i++){
-            JPanel gamePanel = new JPanel();
+            JPanel gameInsidePanel = new JPanel();
+            JPanel gameOutsidePanel = new JPanel();
             JPanel gameNbPanel = new JPanel();
             JPanel gameTitlePanel = new JPanel();
             JPanel buttonChosePanel = new JPanel();
             JPanel buttonModifyPanel = new JPanel();
             JPanel buttonDeletePanel = new JPanel();
+
+            gameInsidePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+            gameInsidePanel.setLayout(new BoxLayout(gameInsidePanel,BoxLayout.LINE_AXIS));
+
+            gameOutsidePanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+            gameOutsidePanel.setLayout((new BoxLayout(gameOutsidePanel, BoxLayout.LINE_AXIS)));
 
             JLabel gameNbLabel = new JLabel("Jeu " + i + " :");
             JLabel gameTitleLabel = new JLabel("Titre"); //fonction pour récupérer le titre du jeu i
@@ -42,45 +62,56 @@ public class GameManagement extends JPanel implements ActionListener {
             buttonDelete.setOpaque(true);
 
             gameNbPanel.add(gameNbLabel, BorderLayout.CENTER);
-            gameNbPanel.setBackground(Color.white);
             gameTitlePanel.add(gameTitleLabel, BorderLayout.CENTER);
-            gameTitlePanel.setBackground(Color.white);
             buttonChosePanel.add(buttonChose, BorderLayout.CENTER);
-            buttonChosePanel.setBackground(Color.white);
             buttonModifyPanel.add(buttonModify, BorderLayout.CENTER);
-            buttonModifyPanel.setBackground(Color.white);
             buttonDeletePanel.add(buttonDelete, BorderLayout.CENTER);
-            buttonDeletePanel.setBackground(Color.white);
 
             buttonChose.addActionListener(this);
             buttonModify.addActionListener(this);
             buttonDelete.addActionListener(this);
 
-            gamePanel.setLayout(new BoxLayout(gamePanel,BoxLayout.LINE_AXIS));
-            gamePanel.add(gameNbPanel);
-            gamePanel.add(gameTitlePanel);
-            gamePanel.add(buttonChosePanel);
-            gamePanel.add(buttonModifyPanel);
-            gamePanel.add(buttonDeletePanel);
+            gameInsidePanel.add(gameNbPanel);
+            gameInsidePanel.add(gameTitlePanel);
+            gameInsidePanel.add(buttonChosePanel);
+            gameInsidePanel.add(buttonModifyPanel);
+            gameInsidePanel.add(buttonDeletePanel);
 
-            gameListPanel.add(gamePanel);
+            gameOutsidePanel.add(gameInsidePanel);
+
+            gameListPanel.add(gameOutsidePanel);
         }
 
         JButton buttonAddGame = new JButton("Créer un nouveau jeu");
-        buttonAddGamePanel.add(buttonAddGame, BorderLayout.CENTER);
-        buttonAddGamePanel.setBackground(Color.white);
         buttonAddGame.addActionListener(this);
-        gameListPanel.add(buttonAddGamePanel);
+        JPanel buttonAddGameInsidePanel = new JPanel();
+        buttonAddGameInsidePanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        buttonAddGameInsidePanel.add(buttonAddGame, BorderLayout.WEST);
+        buttonAddGamePanel.setBorder(BorderFactory.createEmptyBorder(10,0,10,0));
+        buttonAddGamePanel.setLayout(new BoxLayout(buttonAddGamePanel, BoxLayout.LINE_AXIS));
+        buttonAddGamePanel.add(buttonAddGameInsidePanel);
+
 
         JButton buttonReturn = new JButton("Retour");
-        buttonReturn.setBackground(Color.blue);
+        buttonReturn.setBackground(ColorPerso.retour);
+        buttonReturn.setForeground(Color.white);
+        buttonReturn.setOpaque(true);
         buttonReturn.addActionListener(this);
-        buttonReturnPanel.add(buttonReturn, BorderLayout.WEST);
+        buttonReturnPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        buttonReturnPanel.add(buttonReturn);
+
+
+        Border mainEdge = BorderFactory.createEmptyBorder(10,10,10,10);
+        this.setBorder(mainEdge);
+
+
 
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         this.add(windowNamePanel);
-        this.add(gameListPanel);
+        this.add(scrollGameListPanel);
+        this.add(buttonAddGamePanel);
         this.add(buttonReturnPanel);
+
     }
 
     public void actionPerformed(ActionEvent e) {
