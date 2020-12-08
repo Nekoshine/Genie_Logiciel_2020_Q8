14ba3313d366c5aa30ea69beb0a6003d1027fd7a
Yann <ydauvin@enssat.fr>
2020-12-08 17:50:54 +0100

    optimisation de code

--- src/view/ColorPerso.java
+++ src/view/ColorPerso.java
@@ -2,14 +2,14 @@ package view;
 
 import java.awt.*;
 
-public class ColorPerso extends Color {
+class ColorPerso extends Color {
 
-    public static ColorPerso choixJeu = new ColorPerso(243,215,39);
-    public static ColorPerso lancement = new ColorPerso(19,165,25);
-    public static ColorPerso gris = new ColorPerso(211,211,211);
-    public static  ColorPerso retour = new ColorPerso(180,40,20);
+    static Color choixJeu = new Color(243,215,39);
+    static Color lancement = new Color(19,165,25);
+    static Color gris = new Color(211,211,211);
+    static  Color retour = new Color(180,40,20);
 
-    public ColorPerso(int r, int g, int b) {
+    ColorPerso(int r, int g, int b) {
         super(r, g, b);
     }
 }
