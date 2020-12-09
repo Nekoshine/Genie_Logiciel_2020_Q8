ba6d773f777ab003840e8c21d004e653311f8c0c
Yann <ydauvin@enssat.fr>
2020-12-09 11:35:46 +0100

    Ajout de la couleur grises par defaut

--- src/view/ColorPerso.java
+++ src/view/ColorPerso.java
@@ -8,6 +8,7 @@ class ColorPerso extends Color {
     static Color lancement = new Color(19,165,25);
     static Color gris = new Color(211,211,211);
     static  Color retour = new Color(180,40,20);
+    static Color grisOriginal = new Color(238,238,238);
 
     ColorPerso(int r, int g, int b) {
         super(r, g, b);
