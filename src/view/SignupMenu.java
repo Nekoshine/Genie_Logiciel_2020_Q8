7ad24cf98367a412e50907431e26497cec929bc7
alan <asignor@enssat.fr>
2020-12-28 12:53:27 +0100

    english translation

--- src/view/SignupMenu.java
+++ src/view/SignupMenu.java
@@ -13,21 +13,21 @@ import javax.swing.*;
 
 public class SignupMenu extends JPanel implements ActionListener {
 
-    private JPanel login;
-    private JPanel mdp;
-    private JPanel key;
-    private JPanel conteneurretour;
+    private JPanel logincontainer;
+    private JPanel passwordcontainer;
+    private JPanel keycontainer;
+    private JPanel backcontainer;
 
-    private JButton confirmation;
-    private JButton retour;
+    private JButton confirm;
+    private JButton back;
 
-    private JTextField saisieidentifiant;
-    private JTextField saisiecle;
-    private JPasswordField saisiemotdepasse;
+    private JTextField idtextfiled;
+    private JTextField keytextfield;
+    private JPasswordField passwordtextfield;
 
-    private JLabel identifiant;
-    private JLabel motdepasse;
-    private JLabel cleadmin;
+    private JLabel id;
+    private JLabel password;
+    private JLabel key;
 
     private GlobalFrame frame;
 
@@ -38,78 +38,80 @@ public class SignupMenu extends JPanel implements ActionListener {
         //creation de la partie login
 
 
-        login = new JPanel();
-        login.setLayout(new FlowLayout(FlowLayout.CENTER,30,0));
+        logincontainer = new JPanel();
+        logincontainer.setLayout(new FlowLayout(FlowLayout.CENTER,30,0));
 
-        identifiant = new JLabel("Identifiant :");
-        saisieidentifiant = new JTextField();
-        saisieidentifiant.setColumns(30);
+        id = new JLabel("Identifiant :");
+        idtextfiled = new JTextField();
+        idtextfiled.setColumns(30);
 
-        login.add(identifiant);
-        login.add(saisieidentifiant);
+        logincontainer.add(id);
+        logincontainer.add(idtextfiled);
 
         //creation de la partie motdepasse
 
-        mdp = new JPanel();
-        mdp.setLayout(new FlowLayout(FlowLayout.CENTER,10,0));
-        motdepasse = new JLabel("Mot de passe :");
-        saisiemotdepasse = new JPasswordField();
-        saisiemotdepasse.setColumns(30);
+        passwordcontainer = new JPanel();
+        passwordcontainer.setLayout(new FlowLayout(FlowLayout.CENTER,10,0));
+        password = new JLabel("Mot de passe :");
+        passwordtextfield = new JPasswordField();
+        passwordtextfield.setColumns(30);
 
-        mdp.add(motdepasse);
-        mdp.add(saisiemotdepasse);
+        passwordcontainer.add(password);
+        passwordcontainer.add(passwordtextfield);
 
         //creation de la partie clé admin
 
-        key = new JPanel();
-        key.setLayout(new FlowLayout(FlowLayout.CENTER,10,0));
-        cleadmin = new JLabel("Clé (compte admin) :");
-        saisiecle = new JTextField();
-        saisiecle.setColumns(27);
+        keycontainer = new JPanel();
+        keycontainer.setLayout(new FlowLayout(FlowLayout.CENTER,10,0));
+        key = new JLabel("Clé (compte admin) :");
+        keytextfield = new JTextField();
+        keytextfield.setColumns(27);
 
-        key.add(cleadmin);
-        key.add(saisiecle);
+        keycontainer.add(key);
+        keycontainer.add(keytextfield);
 
         //creation du bouton de confirmation
 
-        confirmation= new JButton("Confirmation");
-        confirmation.addActionListener(this);
-        confirmation.setBackground(ColorPerso.vert);
+        confirm = new JButton("Confirmation");
+        confirm.addActionListener(this);
+        confirm.setBackground(ColorPerso.vert);
 
         //creation du bouton de retour
 
-        retour = new JButton("Retour");
-        retour.addActionListener(this);
-        retour.setBackground(ColorPerso.rouge);
-        retour.setForeground(Color.WHITE);
+        back = new JButton("Retour");
+        back.addActionListener(this);
+        back.setBackground(ColorPerso.rouge);
+        back.setForeground(Color.WHITE);
 
-        conteneurretour = new JPanel();
-        conteneurretour.setLayout(new FlowLayout(FlowLayout.LEFT));
-        conteneurretour.add(retour);
+        backcontainer = new JPanel();
+        backcontainer.setLayout(new FlowLayout(FlowLayout.LEFT));
+        backcontainer.add(back);
+
+        //ajout des composants
 
         this.add(Box.createRigidArea(new Dimension(0, 150)));
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        login.setMaximumSize(new Dimension(800,50));
-        this.add(login);
+        logincontainer.setMaximumSize(new Dimension(800,50));
+        this.add(logincontainer);
         this.add(Box.createRigidArea(new Dimension(0, 20)));
-        mdp.setMaximumSize(new Dimension(800,30));
-        this.add(mdp);
+        passwordcontainer.setMaximumSize(new Dimension(800,30));
+        this.add(passwordcontainer);
         this.add(Box.createRigidArea(new Dimension(0, 20)));
         //key.setMaximumSize(new Dimension(1000,30));
-        this.add(key);
+        this.add(keycontainer);
         this.add(Box.createRigidArea(new Dimension(0, 10)));
-        this.add(confirmation);
+        this.add(confirm);
         this.add(Box.createRigidArea(new Dimension(0, 100)));
-        this.add(conteneurretour);
+        this.add(backcontainer);
         this.setVisible(true);
 
     }
 
     public void actionPerformed(ActionEvent event) {
-        if (event.getSource() == confirmation){
-            String idinput = saisieidentifiant.getText();
-            String mdpinput = saisiemotdepasse.getText();
-            String cleinmput = saisiecle.getText();
+        if (event.getSource() == confirm){
+            String idinput = idtextfiled.getText();
+            String mdpinput = passwordtextfield.getText();
+            String cleinmput = keytextfield.getText();
 
             if (idinput.isEmpty() || mdpinput.isEmpty() ){
                 JOptionPane.showMessageDialog(frame,"Un ou plusieurs champs n'ont pas été remplis","Informations incomplètes", JOptionPane.WARNING_MESSAGE);
@@ -123,7 +125,7 @@ public class SignupMenu extends JPanel implements ActionListener {
             }
             else if (!(cleinmput.isEmpty())){}
         }
-        else if (event.getSource() == retour){
+        else if (event.getSource() == back){
             frame.connectionMenuDisplay(frame);
         }
     }
