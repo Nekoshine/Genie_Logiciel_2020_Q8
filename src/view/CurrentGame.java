9f5aa4cea3e3a02adcf0b3d878b118344a4ce747
alan <asignor@enssat.fr>
2020-12-28 12:54:44 +0100

    creation de la fonction d'affichage pour les joueurs

---
+++ src/view/CurrentGame.java
@@ -0,0 +1,14 @@
+package view;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class CurrentGame extends JPanel implements ActionListener {
+
+
+
+    public void actionPerformed(ActionEvent event) {}
+}
