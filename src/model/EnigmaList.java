3546bae8fbd9e72edc3fd2ae76725404909e9293
Yann <ydauvin@enssat.fr>
2020-12-29 19:34:49 +0100

    nouveau aaddEnigma

--- src/model/EnigmaList.java
+++ src/model/EnigmaList.java
@@ -8,30 +8,33 @@ public class EnigmaList {
 
     public ArrayList<Enigma> List;
 
-    public EnigmaList(){
+    public EnigmaList() {
         List = new ArrayList<>();
     }
 
-    public void addEnigma(Enigma enigma){
+    public void addEnigma(Enigma enigma) {
         List.add(enigma);
     }
 
-    public void addEnigma(int id,int idUser,String text,String answer,String clue1,int timer1,String clue2,int timer2,String clue3,int timer3){
-        List.add(new Enigma(id,idUser,text,answer,clue1,timer1,clue2,timer2,clue3,timer3));
+    public void addEnigma(int id, int idUser, String text, String answer, String clue1, int timer1, String clue2, int timer2, String clue3, int timer3) {
+        List.add(new Enigma(id, idUser, text, answer, clue1, timer1, clue2, timer2, clue3, timer3));
     }
 
-    public Enigma getEnigma(int i){
+    public void addEnigma(int id, String text, String answer, String clue1, int timer1, String clue2, int timer2, String clue3, int timer3) {
+        List.add(new Enigma(id, text, answer, clue1, timer1, clue2, timer2, clue3, timer3));
+    }
+
+    public Enigma getEnigma(int i) {
         return List.get(i);
     }
 
-    public void removeEnigma(int i){List.remove(i);}
+    public void removeEnigma(int i) {
+        List.remove(i);
+    }
 
 
-    public int getSize(){
+    public int getSize() {
         return List.size();
     }
 
-    public Enigma getEnigma(int i){
-        return List.get(i);
-    }
 }
