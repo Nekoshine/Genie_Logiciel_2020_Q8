e6ebfaa28b9e92e95db011325d10988792f1eb28
Yann <ydauvin@enssat.fr>
2020-12-31 12:35:57 +0100

    initialisation si pas de connecction

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -1,6 +1,7 @@
 package launcher;
 
 import database.DBEnigma;
+import database.DBRoom;
 import model.*;
 import view.GlobalFrame;
 
@@ -9,11 +10,12 @@ import java.io.IOException;
 public class Main {
     public static EnigmaList ListEnigma = new EnigmaList();
     public static int idUser=3;
+    public static RoomList ListRoom = new RoomList();
+
     public static void main(String[] args) throws IOException {
 
     ListEnigma= DBEnigma.getEnigmas(1);
-
-
+    ListRoom = DBRoom.getRooms(idUser);
         /* Création de la vue */
         new GlobalFrame();
 
