1b06397dad310877f14d7b3fd728ba1dbd04cd3c
Yann <ydauvin@enssat.fr>
2021-01-08 13:44:10 +0100

    Test sur la BDD room

---
+++ test/java/database/DBRoomTest.java
@@ -0,0 +1,63 @@
+package database;
+
+import model.Room;
+import model.RoomList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBRoomTest {
+    private final static int idGame = 2;
+
+    @Test
+    final void testInsertRoom() {
+        boolean test = DBRoom.insertRoom(DBRoom.getMax()+1,idGame);
+        Assertions.assertTrue(test, "Echec de l'ajout de la room");
+    }
+
+    @Test
+    final void testgetMax() {
+        boolean test=false;
+        int max = DBRoom.getMax();
+        int maxBDD = 13; //à lire direct dans la bdd
+        if(max==maxBDD){
+            test = true;
+        }
+        Assertions.assertTrue(test, "Echec recuperation max");
+    }
+
+    @Test
+    final void testGetRooms() {
+        int id = DBRoom.getMax()+1;
+        boolean test=false;
+        boolean insert = DBRoom.insertRoom(id,idGame);
+        if(insert){
+            RoomList listRoom = DBRoom.getRooms(3);
+            Room room = listRoom.getRoom(listRoom.getSize()-1);
+            if (id== room.getId() && idGame==room.getGame().getId()){
+                test=true;
+            }
+            Assertions.assertTrue(test, "Echec de la lecture");
+
+        }
+        else {
+            Assertions.assertTrue(insert, "Echec de l'ajout");
+        }
+    }
+
+    @Test
+    final void TestMajJeu(){
+        int id = DBRoom.getMax()+1;
+        boolean test=false;
+        boolean insert = DBRoom.insertRoom(id,idGame);
+        if(insert) {
+            test = DBRoom.majJeu(id, 3);
+            Assertions.assertTrue(test, "Echec de la mise a jour");
+        }
+        else {
+            Assertions.assertTrue(insert, "Echec de l'ajout");
+        }
+    }
+
+}
