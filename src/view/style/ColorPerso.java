1a9d4fa57f290aacec1a27bf52ae56e10796288e
Yann <ydauvin@enssat.fr>
2021-01-23 12:03:57 +0100

    petit changement de couleur

--- src/view/style/ColorPerso.java
+++ src/view/style/ColorPerso.java
@@ -4,22 +4,21 @@ import java.awt.*;
 
 public class ColorPerso extends Color {
 
-    public static Color jaune = new Color(237,219,0);
-    public static Color jauneHoover = new Color(255, 255, 0);
+    public static Color jaune = new Color(224,206,0);
+    public static Color jauneHoover = new Color(212, 194, 0);
 
     public static Color vert = new Color(19,165,25);
-    public static Color vertHoover = new Color(0, 255, 0);
+    public static Color vertHoover = new Color(17, 140, 21);
 
     public static Color grisClair = new Color(211,211,211);
     public static Color grisFonce = new Color(100,100,100);
-
-    public static Color rouge = new Color(180,40,20);
-    public static Color rougeHoover = new Color(255, 0, 0);
-
     public static Color grisOriginal = new Color(238,238,238);
 
+    public static Color rouge = new Color(180,40,20);
+    public static Color rougeHoover = new Color(156,36,17);
+
     public static Color azur = new Color(30,127,203);
-    public static Color azurHoover = new Color(0, 0, 255);
+    public static Color azurHoover = new Color(27, 113, 179);
 
     ColorPerso(int r, int g, int b) {
         super(r, g, b);
