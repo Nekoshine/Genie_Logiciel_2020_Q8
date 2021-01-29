3af0b9bfa7bdab591bb48676bbdb9cc36b18c30a
Baptiste Geslot <bgeslot@gmail.com>
2021-01-29 11:48:37 +0100

    Javadoc du score

--- src/model/Score.java
+++ src/model/Score.java
@@ -9,36 +9,72 @@ public final class Score{
       private int idGame;
       private int score;
 
+    /**
+     * Création d'un objet Score
+     * @param id L'id du Score dans la base de donnée
+     * @param idGame L'id du jeu qui est lié à ce score
+     * @param idUser L'id de l'utilisateur qui a marqué ce score
+     * @param score  La valeur du score
+     */
       public Score(int id,int idGame,int idUser,int score){
             this.id = id;
             this.idGame = idGame;
             this.idUser=idUser;
             this.score=score;
       }
+
+    /**
+     * Fonction qui permet d'obtenir l'id du score
+     * @return L'id du score
+     */
       public int getId(){
           return this.id;
       }
 
-      public int getIdUser(){
+    /**
+     * Fonction qui permet d'obtenir l'id de l'utilisateur qui a marqué ce score
+     * @return  l'id de l'utilisateur qui a marqué ce score
+     */
+    public int getIdUser(){
           return this.idUser;
       }
 
+    /**
+     * Fonction qui permet d'obtenir l'id du jeu
+     * @return  l'id du jeu
+     */
       public int getIdGame(){
           return this.idGame;
       }
 
+    /**
+     * Fonction qui permet d'obtenir la valeur du score
+     * @return  la valeur du score
+     */
       public int getScore(){
           return this.score;
       }
 
+    /**
+     *  Permet de définir l'id d'un score
+     * @param id  l'id du score
+     */
       public void setId(int id) {
           this.id = id;
       }
 
+    /**
+     * Permet de définir l'id de l'user
+     * @param idUser l'id de l'user
+     */
       public void setIdUser(int idUser) {
           this.idUser = idUser;
       }
 
+    /**
+     * Permet de définir la valeur du score
+     * @param score la valeur du score
+     */
       public void setScore(int score) {
           this.score = score;
       }
